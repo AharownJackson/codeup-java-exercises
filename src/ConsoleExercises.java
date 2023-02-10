2e1a6211f6a6e292d8ad1064affec7f23ca1bc06
aharownjackson <aharownmjackson@gmail.com>
2023-02-10 10:59:56 -0600

    problem 1

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately: %.2f.", pi);
+//        String message = String.valueOf(System.out.format("The value of pi is approximately: %.2s.", pi) );
+//        System.out.println(message);
+
+
+//        Scanner dasScanner = new Scanner(System.in);
+//        System.out.println("Please enter an integer between -128 and 127:  ");
+//        int userInput = dasScanner.nextInt();
+//        System.out.println(userInput);
+
+//        Scanner dasScanner = new Scanner(System.in);
+
+//        System.out.println("Please enter three words: ");
+//
+//        String word1 = dasScanner.nextLine();
+//        String word2 = dasScanner.nextLine();
+//        String word3 = dasScanner.nextLine();
+//
+//        System.out.printf("%s%n%s%n%s", word1, word2, word3);
+
+//        System.out.println(wor
+
+//        Scanner dasScanner = new Scanner(System.in);
+//        System.out.println("Please enter the length of the classroom in feet: ");
+//        String userLength = dasScanner.nextLine();
+//        double length = Double.parseDouble(userLength);
+//        System.out.println("Please enter the width of the classroom in feet: ");
+//        String userWidth = dasScanner.nextLine();
+//        double width = Double.parseDouble(userWidth);
+//
+//        double area = length * width;
+//        double perimeter = (length + width) * 2;
+//
+//        System.out.printf("The area of the classroom is: %.2f%n", area);
+//        System.out.printf("The perimeter of the classroom is: %.2f", perimeter);
+
+
+
+
+    }
+}
