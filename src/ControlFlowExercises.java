4600464316ed657e47b8c6bce0ec01ba73061ea4
aharownjackson <aharownmjackson@gmail.com>
2023-02-10 11:02:10 -0600

    finished problem 1b

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-       int i = 5;
+       int i = 0;
        do {
            System.out.println(i);
-           i+=1;
-       } while (i <= 15);
+           i += 2;
+       } while  (i <= 100);
+
     }
 }
