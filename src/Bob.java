2fef21ef5184ce9e1930d91e06c5e051ebfd99e4
aharownjackson <aharownmjackson@gmail.com>
2023-02-12 09:21:21 -0600

    finished convo with Bob

--- src/Bob.java
+++ src/Bob.java
@@ -3,23 +3,51 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner derScanner = new Scanner(System.in);
-        System.out.println("Bob is a lackadaisical teenager. In conversation, his responses are very limited. Please ask Bob a question.");
-        String question = derScanner.nextLine();
-        if(question.endsWith("?")){
-            System.out.println("Sure.");
-        } else if(question.endsWith("!")){
-            System.out.println("Whoa, chill out!");
-        } else if(question.isEmpty()){
-            System.out.println("Fine. Be that way!");
-        } else {
-            System.out.println("Whatever.");
-        }
-        System.out.println("Do you want to continue: [y/N]");
-        String userInput = derScanner.nextLine();
-        if(userInput.equals("y")){
-            // run the program again.
-        }
+        System.out.println("Bob is a lackadaisical teenager. In conversation, his responses are very limited.");
+        for (int i = 0; i <= 1; i += 0) {
+            System.out.println("Please ask Bob a question.");
+            String question = derScanner.nextLine();
+            if (question.endsWith("?")) {
+                System.out.println("Sure.");
+                System.out.println("Do you want to ask Bob another question? [y/N]");
+                String userInput = derScanner.nextLine();
+                if (userInput.equals("y")) {
+                    i += 0;
+                } else {
+                    i += 1;
+                }
+
+            } else if (question.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+                System.out.println("Do you want to ask Bob another question? [y/N]");
+                String userInput = derScanner.nextLine();
+                if (userInput.equals("y")) {
+                    i += 0;
+                } else {
+                    i += 1;
+                }
+
+            } else if (question.isEmpty()) {
+                System.out.println("Fine. Be that way!");
+                System.out.println("Do you want to ask Bob another question? [y/N]");
+                String userInput = derScanner.nextLine();
+                if (userInput.equals("y")) {
+                    i += 0;
+                } else {
+                    i += 1;
+                }
 
 
+            } else {
+                System.out.println("Whatever.");
+                System.out.println("Do you want to ask Bob another question? [y/N]");
+                String userInput = derScanner.nextLine();
+                if (userInput.equals("y")) {
+                    i += 0;
+                } else {
+                    i += 1;
+                }
+            }
+        }
     }
 }
