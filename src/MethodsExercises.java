a2f57053091723ed5bcc58959ff755c819964491
aharownjackson <aharownmjackson@gmail.com>
2023-02-13 16:46:47 -0600

    end of day commit

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,52 +1,105 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
 
-    public static int addition(int num1, int num2) {
-        return num1 + num2;
-    }
+    //    problem 1
+//    public static int addition(int num1, int num2) {
+//        return num1 + num2;
+//    }
+//
+//    public static int subtraction(int num1, int num2) {
+//        return num1 - num2;
+//    }
+//
+//    public static int multiplication(int num1, int num2) {
+//        return num1 * num2;
+//    }
+//
+//    public static int multiplicationLoop(int num, int times) {
+//        int result = 0;
+//        for (int i = 0; i < times; i += 1) {
+//            result += num;
+//        }
+//        return result;
+//    }
+//
+//    public static int recursionMultiplication(int num, int times) {
+//        int result = 0;
+//        if (times > 0) {
+//            result = multiplicationLoop(num, times);
+//        }
+//        return result;
+//    }
+//
+//    public static int division(int num1, int num2) {
+//        return num1 / num2;
+//    }
+//
+//    public static int modulus(int num1, int num2) {
+//        return num1 % num2;
+//    }
 
-    public static int subtraction(int num1, int num2) {
-        return num1 - num2;
-    }
+    //    problem 2
+    public static int getInteger(int min, int max) {
 
-    public static int multiplication(int num1, int num2) {
-        return num1 * num2;
-    }
+        System.out.printf("""
+                Enter a number between %d and %d.
+                """, min, max);
 
-    public static int multiplicationLoop(int num, int times) {
-        int result = 0;
-        for (int i = 0; i < times; i += 1) {
-            result += num;
+        Scanner derScanner = new Scanner(System.in);
+
+        int userInput = derScanner.nextInt();
+
+        if(userInput < min || userInput > max) {
+            System.out.println("Bad number.");
+            int userInt = getInteger(min, max);
+
         }
-        return result;
+        return userInput;
+
     }
 
-    public static int recursionMultiplication(int num, int times) {
-        int result = 0;
-        if (times > 0) {
-          result = multiplicationLoop(num, times);
-        }
-        return result;
-    }
+//    problem 3
+//    public static void getFactorial (int num) {
+//        Scanner derScanner = new Scanner(System.in);
+//        int runner = 0;
+//        while (runner == 0) {
+//            System.out.println("""
+//                Please enter a number between 1 and 10.
+//                """);
+//            int userInt = derScanner.nextInt();
+//            if(userInt >= 1 && userInt <= 10) {
+//                System.out.println("""
+//                        do you want to continue? [1 for Yes / 2 for No]
+//                        """);
+//                int q = derScanner.nextInt();
+//                if(q == 1){
+//                    userInt!;
+//                } else {
+//                }
+//            }
+//
+//        }
+//    }
 
-    public static int division(int num1, int num2) {
-        return num1 / num2;
-    }
-
-    public static int modulus(int num1, int num2) {
-        return num1 % num2;
-    }
+//    problem 5
 
 
-//    logging
+
+
+
+    //    logging
     public static void main(String[] args) {
 
-        System.out.println(addition(10, 2));
-        System.out.println(subtraction(10, 2));
-        System.out.println(multiplication(10, 2));
-        System.out.println(division(10, 2));
-        System.out.println(modulus(10, 2));
-        System.out.println(multiplicationLoop(10, 2));
-        System.out.println(recursionMultiplication(10, 2));
+//        System.out.println(addition(10, 2));
+//        System.out.println(subtraction(10, 2));
+//        System.out.println(multiplication(10, 2));
+//        System.out.println(division(10, 2));
+//        System.out.println(modulus(10, 2));
+//        System.out.println(multiplicationLoop(10, 2));
+//        System.out.println(recursionMultiplication(10, 2));
+
+        getInteger(0, 100);
 
     }
 }
--- src/Practice.java
+++ src/Practice.java
@@ -15,8 +15,93 @@ public class Practice {
     //    public static int returnSumOfNums (int num1, int num2, int num3){
 //        return num1 + num2 + num3;
 //    }
+
+    public static void sayName(String name) {
+        System.out.printf("""
+                Hello %s, how are you today?
+                """, name);
+    }
+    public static void sayName(String first, String last) {
+        System.out.printf("""
+                Hello %s %s, how are you today?
+                """, first, last);
+    }
+
+
+
+
+// TODO: overload the math exercises from the curriculum exercise to work with both integers and doubles
+
+    public static int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+    public static double addition(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+    public static double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+    public static double multiplication(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static int multiplicationLoop(int num, int times) {
+        int result = 0;
+        for (int i = 0; i < times; i += 1) {
+            result += num;
+        }
+        return result;
+    }
+    public static double multiplicationLoop(double num, double times) {
+        int result = 0;
+        for (int i = 0; i < times; i += 1) {
+            result += num;
+        }
+        return result;
+    }
+
+    public static int recursionMultiplication(int num, int times) {
+        int result = 0;
+        if (times > 0) {
+            result = multiplicationLoop(num, times);
+        }
+        return result;
+    }
+    public static double recursionMultiplication(double num, double times) {
+        int result = 0;
+        if (times > 0) {
+//            result = multiplicationLoop(num, times);
+        }
+        return result;
+    }
+
+    public static int division(int num1, int num2) {
+        return num1 / num2;
+    }
+    public static double division(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    public static int modulus(int num1, int num2) {
+        return num1 % num2;
+    }
+    public static double modulus(double num1, double num2) {
+        return num1 % num2;
+    }
+
     public static void main(String[] args) {
         printThanks();
+
+        sayName("Aharown");
+        sayName("Aharown", "Jackson");
 
 
 
