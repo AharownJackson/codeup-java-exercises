a523308cdae964d63dcb0af914e7284f274a7305
aharownjackson <aharownmjackson@gmail.com>
2023-02-17 09:16:38 -0600

    finished random server name generator

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,3 +1,5 @@
+import utils.Input;
+
 public class ServerNameGenerator {
     public static String[] adjectives = new String[] {
             "Fast",
@@ -13,15 +15,15 @@ public class ServerNameGenerator {
     };
 
     public static String[] nouns = new String[] {
-            "German Shepard",
-            "Fire Truck",
+            "German-Shepard",
+            "Fire-Truck",
             "Olympics",
-            "Adventure Canoe",
-            "Apple Watch",
-            "Water Bottle",
-            "Lunch Box",
-            "Toilet Paper",
-            "Blue Light glasses",
+            "Adventure-Canoe",
+            "Apple-Watch",
+            "Water-Bottle",
+            "Lunch-Box",
+            "Toilet-Paper",
+            "Blue-Light-glasses",
             "Coyote"
     };
 
@@ -31,6 +33,31 @@ public class ServerNameGenerator {
     public static String getRandomWord(String[] words) {
         int randomNumber =  (int) (Math.random() * words.length);
         return words[randomNumber];
+    }
+
+    public static String serverNameGenerator() {
+        Input input = new Input();
+        System.out.println("Do you want to generate a randomized server name?");
+        boolean userCont = input.yesNo();
+        String wordOne = null;
+        String wordTwo = null;
+        String serverName = null;
+        if (userCont) {
+            wordOne = getRandomWord(adjectives);
+            wordTwo = getRandomWord(nouns);
+            serverName = wordOne + "-" + wordTwo;
+        }
+        return serverName;
+    }
+
+    public static void main(String[] args) {
+        String userServerName = serverNameGenerator();
+
+        System.out.printf("""
+                Here is your server name:
+                %s
+                """, userServerName);
+
 
     }
 }
