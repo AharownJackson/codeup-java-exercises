8746795f43337728e95c6ecfa5d24fc8eb2ea211
aharownjackson <aharownmjackson@gmail.com>
2023-02-21 10:16:01 -0600

    finished movie application

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,5 +1,9 @@
 package movies;
 
+import utils.Input;
+
+import static movies.MoviesArray.findAll;
+
 public class Movie {
     private String name;
     private String category;
@@ -35,7 +39,104 @@ public class Movie {
     }
 
     // METHODS //
-//    public static
 
+    // runs the movie application //
+    public static void runMoviesApp() {
+        Input input = new Input();
+        System.out.println("""
+                Please choose an option:
+                ------------------------
+                1 - view all
+                2 - view by category
+                """);
+        int userMainInput = input.getInt();
+        if (userMainInput == 2) {
+            generateCategories();
+        } else {
+            Movie[] userChoice = findAll();
+            for (Movie movie : userChoice) {
+                System.out.println(movie.getName());
+                System.out.println("Continue?");
+                boolean userInput = input.yesNo();
+                if (userInput) {
+                    runMoviesApp();
+                }
+            }
+        }
+    }
+
+    // generates the movie category search menu and functionality //
+    public static void generateCategories() {
+        Input input = new Input();
+        System.out.println("""
+                Please choose a category:
+                ------------------------
+                1 - animated
+                2 - drama
+                3 - horror
+                4 - sci-fi
+                5 - go back
+                """);
+        int userInt = input.getInt();
+        if (userInt == 1) {
+            Movie[] userChoice = findAll();
+            for (Movie movie : userChoice) {
+                if (movie.category.equals("animated")) {
+                    System.out.println(movie.getName());
+                    System.out.println("Continue?");
+                    boolean userInput = input.yesNo();
+                    if (userInput) {
+                        runMoviesApp();
+                    }
+                }
+            }
+        }
+        if (userInt == 2) {
+            Movie[] userChoice = findAll();
+            for (Movie movie : userChoice) {
+                if (movie.category.equals("drama")) {
+                    System.out.println(movie.getName());
+                    System.out.println("Continue?");
+                    boolean userInput = input.yesNo();
+                    if (userInput) {
+                        runMoviesApp();
+                    }
+                }
+            }
+        }
+        if (userInt == 3) {
+            Movie[] userChoice = findAll();
+            for (Movie movie : userChoice) {
+                if (movie.category.equals("horror")) {
+                    System.out.println(movie.getName());
+                    System.out.println("Continue?");
+                    boolean userInput = input.yesNo();
+                    if (userInput) {
+                        runMoviesApp();
+                    }
+                }
+            }
+        }
+        if (userInt == 4) {
+            Movie[] userChoice = findAll();
+            for (Movie movie : userChoice) {
+                if (movie.category.equals("sci-fi")) {
+                    System.out.println(movie.getName());
+                    System.out.println("Continue?");
+                    boolean userInput = input.yesNo();
+                    if (userInput) {
+                        runMoviesApp();
+                    }
+                }
+            }
+        }
+        if (userInt == 5) {
+            runMoviesApp();
+        }
+    }
 
 }
+
+
+
+
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -4,32 +4,15 @@ import utils.Input;
 
 import java.util.Arrays;
 
+import static movies.Movie.runMoviesApp;
 import static movies.MoviesArray.findAll;
 
 public class MoviesApplication {
     public static void main(String[] args) {
-        Input input = new Input();
-        System.out.println("""
-                Please choose an option:
-                ------------------------
-                1 - view all
-                2 - view by category
-                """);
-        int userMainInput = input.getInt();
-        if(userMainInput == 2) {
-            System.out.println("""
-                    Please choose a category:
-                    ------------------------
-                    1 - view movies in the animated category
-                    2 - view movies in the drama category
-                    3 - view movies in the horror category
-                    4 - view movies in the sci-fi category
-                    5 - go back
-                    """);
-        } else {
-            System.out.println(Arrays.toString(findAll()));
-        }
-        int userCategoryInput = input.getInt();
+
+        runMoviesApp();
+
+
 
 
     }
